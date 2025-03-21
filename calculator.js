0cc18e2d3a0851058b2bf56e6b7ff0b655e63365
Tadi Mhizha <151214925+Tadi263@users.noreply.github.com>
2025-03-21 19:53:20 +0000

    added functionality to both input and outout displays and incorparated calc. fucntion on UI

--- calculator.js
+++ calculator.js
@@ -1,41 +1,78 @@
-// function operator(){
-//     let input = prompt('input please format: "(number) (operater) (number)"', )
-//     let inputArray = input.split("").join("").split(" ");
-//     let a = inputArray[0];
-//     let b = inputArray[2];
-//     let operatorSymbol = inputArray[1];
+let number1 = 0;
+let number2 = 0;
+let operatorSymbol = '';
 
-//     function add(a,b){
-//         alert(parseInt(a) + parseInt(b));
-//       };
+function operator(){
 
-//     function subtract(a,b){
-//         alert(parseInt(a) - parseInt(b));
-//     };
+     let a = number1;
+     let b = number2;
 
-//     function multiply(a,b) {
-//         alert(parseInt(a) * parseInt(b));
-//       };
+    function add(a,b){
+        inputField.value = (parseInt(a) + parseInt(b));
+        console.log(parseInt(a) + parseInt(b));
+      };
 
-//     function divide(a,b){
-//         alert(parseInt(a) / parseInt(b));
-//     };
+    function subtract(a,b){
+        inputField.value = (parseInt(a) - parseInt(b));
+        console.log(parseInt(a) - parseInt(b));
+    };
 
-//     if (operatorSymbol == '+'){
-//         add(a,b);
-//     }
-//     else if (operatorSymbol == '-'){
-//         subtract(a,b);
-//     }
-//     else if (operatorSymbol == '*'){
-//         multiply(a,b);
-//     }
-//     else if (operatorSymbol == '/'){
-//         divide(a,b);
-//     }
-// }
+    function multiply(a,b) {
+        inputField.value = (parseInt(a) * parseInt(b));
+        console.log(parseInt(a) * parseInt(b));
+      };
 
-// operator();
+    function divide(a,b){
+        inputField.value = (parseInt(a) / parseInt(b));
+        console.log(parseInt(a) / parseInt(b));
+    };
+
+    if (operatorSymbol == '+'){
+        add(a,b);
+    }
+    else if (operatorSymbol == '-'){
+        subtract(a,b);
+    }
+    else if (operatorSymbol == '*'){
+        multiply(a,b);
+    }
+    else if (operatorSymbol == '/'){
+        divide(a,b);
+    }
+}
 
 
+const btns = document.querySelectorAll('button[class~="numbers"]');
 
+const inputField = document.querySelector('#input-display');
+
+const outputField = document.querySelector('#output-display');
+
+const operaters =  document.querySelectorAll('button[class~="operators"]');
+
+function updateInputBox(event) {
+    inputField.value += event.target.id;  // Get the id of the clicked button
+}
+
+// Loop through each button and add event listener
+btns.forEach(btn => {
+    btn.addEventListener("click", updateInputBox);
+});
+
+operaters.forEach(operater => {
+    operater.addEventListener("click", () => {
+                if (operater.id == '='){
+                    number2 = inputField.value;
+                    outputField.innerHTML += ' ' + number2 + ' ' + '=';
+                    operator()
+                    return false;
+                }
+            number1 = inputField.value;
+            operatorSymbol = operater.id;
+            inputField.value = '';
+            outputField.innerHTML = number1 + ' ' + operater.textContent;
+            console.log(number1);
+            console.log(operatorSymbol);
+            console.log(inputField.value);}
+        );
+});
